afd70d41c68f83caa0f85e10d0e52fe98f13db2f
andrea-maiolo <maiolo.m.2@gmail.com>
2022-01-13 21:48:04 +0100

    started working on design

--- src/components/Main-container.js
+++ src/components/Main-container.js
@@ -3,14 +3,19 @@ import React from "react";
 function Main(props){
     return(
         <div className="experience-container">
-            <img src={props.imageUrl} />
+            <img className="pictures" src={props.imageUrl} />
             <div className='experience-description'>
-                <p>{props.location}</p>
-                <a>{props.googleMapsUrl} </a>
-                <h2>{props.title}</h2>
-                <p>{props.startDate}</p>
-                <p>{props.endDate}</p>
-                <p>{props.description}</p>
+                <div className="locationStuff">
+                    <img className="locationLogo" src={require('./../images/location.png')} />
+                    <p className="location">{props.location}</p>
+                    <a herf={props.googleMapsUrl} className="maps">View on Google Maps</a>
+                </div>
+                <h2 className="experience-title">{props.title}</h2>
+                <div className='dateStuff'>
+                    <p className="startDate">{props.startDate}</p> -
+                    <p className="endDate">{props.endDate}</p>
+                </div>
+                <p className="experience-description">{props.description}</p>
             </div>
         </div>
     )
